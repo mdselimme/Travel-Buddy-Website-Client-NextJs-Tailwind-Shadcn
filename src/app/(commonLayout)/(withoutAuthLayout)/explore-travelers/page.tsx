2355577939a4825d696be857bd95f7c49cbf01d1
mdselimme <mdselimdev@gmail.com>
2025-12-09 15:50:08 +0600

    added: with auth page and dashboard layout

---
+++ src/app/(commonLayout)/(withoutAuthLayout)/explore-travelers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExploreTravelersPage = () => {
+  return <div>ExploreTravelersPage</div>;
+};
+
+export default ExploreTravelersPage;
---
+++ src/app/(commonLayout)/(withoutAuthLayout)/find-travel-buddy/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FindTravelBuddy = () => {
+  return <div>FindTravelBuddy</div>;
+};
+
+export default FindTravelBuddy;
