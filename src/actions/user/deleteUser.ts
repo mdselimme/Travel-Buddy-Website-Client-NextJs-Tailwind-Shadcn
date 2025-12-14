16148843960e56bb2110cfdc8051737b42330f76
mdselimme <mdselimdev@gmail.com>
2025-12-14 23:56:28 +0600

    added delete user data action

---
+++ src/actions/user/deleteUser.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { serverFetch } from "@/lib/serverFetch";
+
+export const deleteUser = async (userId: string) => {
+    try {
+        const response = await serverFetch.delete(`/user/${userId}`);
+
+        const data = await response.json()
+
+        if (!response.ok || !data.success) {
+            throw new Error(data.message || "User not deleted.")
+        };
+
+        return data.message || "User deleted successfully."
+    } catch (error) {
+        throw new Error(error instanceof Error ? error.message : "User not deleted.")
+    }
+};
