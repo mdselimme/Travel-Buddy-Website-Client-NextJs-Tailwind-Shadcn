0cdfd3a291cf51ac5397c3bc191af998e3d37e2a
mdselimme <mdselimdev@gmail.com>
2026-01-19 01:57:39 +0600

    fix: my profile get all travel plan user

--- src/app/(dashboardLayout)/(commonProtectedLayout)/my-profile/page.tsx
+++ src/app/(dashboardLayout)/(commonProtectedLayout)/my-profile/page.tsx
@@ -1,11 +1,11 @@
 import { getMeProfile } from "@/actions/profile/getMeProfile";
-import { getAllTravelType } from "@/actions/travelType/getAllTravelType";
 import { getUserInfo } from "@/actions/user/getUserInfo";
 import MyProfile from "@/components/modules/MyProfile/MyProfile";
 import { IProfile } from "@/types/profile.types";
 import { ITravelType } from "@/types/travel.type";
 import { IUser } from "@/types/user.types";
 import { Metadata } from "next";
+import { getAllTravelTypeUsers } from "../../../../actions/travelType/getAllTravelTypeUsers";
 
 export const metadata: Metadata = {
   title: `My Profile || Travel Buddy`,
@@ -16,12 +16,12 @@ export const metadata: Metadata = {
 const MyProfilePage = async () => {
   const profile = await getMeProfile();
   const userData = await getUserInfo();
-  const travelTypes = await getAllTravelType();
+  const travelTypes = await getAllTravelTypeUsers();
   return (
     <MyProfile
       profileData={profile.data as IProfile}
       userInfo={userData as IUser}
-      travelTypes={travelTypes.data as ITravelType[]}
+      travelTypes={travelTypes as ITravelType[]}
     />
   );
 };
